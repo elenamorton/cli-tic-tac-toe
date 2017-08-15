eea6898e2a3fcd8cc09bc43a5ba8ef35c7eb051e
Elena Morton <elena.uleia@gmail.com>
2017-08-15 15:38:22 +0000

    adding simple coverage to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,7 +11,16 @@
 # a separate helper file that requires the additional dependencies and performs
 # the additional setup, and require it from the spec files that actually need
 # it.
-#
+
+require 'simplecov'
+require 'simplecov-console'
+require 'rspec'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+])
+SimpleCov.start
+
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
