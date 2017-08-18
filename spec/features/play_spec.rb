f4b3286ef825a059e204cfa91169652dfc5bbc3e
Elena Morton <elena.uleia@gmail.com>
2017-08-18 09:35:15 +0000

    realignement for 2 spaces instead of 4

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -7,29 +7,30 @@ require 'stringio'
 
 describe Game do
     
-    def io(input='')
-       incoming = StringIO.new(input)
-       @outgoing = StringIO.new
-       described_class.new(@outgoing, incoming) 
-    end
+  def io(input='')
+    incoming = StringIO.new(input)
+    @outgoing = StringIO.new
+    described_class.new(@outgoing, incoming) 
+  end
     
-    def output
-        @outgoing.string
-    end
+  def output
+    @outgoing.string
+  end
 
-    it 'play a full game, user starts in a corner' do
-        io("8\n6\n1\n5\n0\n3\n").start_game
-        expect(output).to include 'O'
-        expect(output).to include 'X'
-        expect(output).to include 'Game over'
-        expect(output).to include "===+===+==="
-    end
+  it 'play a full game, user starts in a corner' do
+    io("8\n6\n1\n5\n0\n3\n").start_game
+    expect(output).to include 'O'
+    expect(output).to include 'X'
+    expect(output).to include 'Game over'
+    expect(output).to include "===+===+==="
+  end
     
-        it 'play a full game, user starts in the center' do
-        io("4\n8\n2\n6\n").start_game
-        expect(output).to include 'O'
-        expect(output).to include 'X'
-        expect(output).to include 'Game over'
-        expect(output).to include "===+===+==="
-    end
+  it 'play a full game, user starts in the center' do
+    io("4\n8\n2\n6\n").start_game
+    expect(output).to include 'O'
+    expect(output).to include 'X'
+    expect(output).to include 'Game over'
+    expect(output).to include "===+===+==="
+  end
+  
 end
