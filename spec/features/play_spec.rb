a76ba927288c7a1153d5c4d613fbc79ff02359b3
Elena Morton <elena.uleia@gmail.com>
2017-08-18 09:50:09 +0000

    adding test for displaying empty board

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -16,13 +16,23 @@ describe Game do
   def output
     @outgoing.string
   end
+  
+  it 'displaying an empty board' do
+    empty_board = Array.new(9) { nil }
+    io.display_board(empty_board)
+    expect(output).to eq "   |   |   \n" \
+                        "===+===+===\n" \
+                        "   |   |   \n" \
+                        "===+===+===\n" \
+                        "   |   |   \n"
+  end
 
   it 'play a full game, user starts in a corner' do
     io("8\n6\n1\n5\n0\n3\n").start_game
     expect(output).to include 'O'
     expect(output).to include 'X'
     expect(output).to include 'Game over'
-    expect(output).to include "===+===+==="
+    expect(output).to include '===+===+==='
   end
     
   it 'play a full game, user starts in the center' do
@@ -30,7 +40,7 @@ describe Game do
     expect(output).to include 'O'
     expect(output).to include 'X'
     expect(output).to include 'Game over'
-    expect(output).to include "===+===+==="
+    expect(output).to include '===+===+==='
   end
   
 end
