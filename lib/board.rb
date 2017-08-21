6af8de9e947eadf09e0941d0326608d7e311635d
Elena Morton <elena.uleia@gmail.com>
2017-08-21 19:36:46 +0000

    correction for number_of_spots, added place_marker method

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,12 @@
 class Board
     
-  attr_reader :width, :number_of_spaces
+  attr_reader :width, :number_of_spots, :number_occupied_spots
     
   def initialize(width)
     @width = width
-    @number_of_spaces = width * width
-    @board = Array.new(number_of_spaces) { |i| i.to_s }
+    @number_of_spots = width * width
+    @number_occupied_spots = 0
+    @board = Array.new(number_of_spots) { |i| i.to_s }
   end
     
     
@@ -13,8 +14,14 @@ class Board
     @board.dup    
   end
   
-  def content_of(space)
-    @board[space]
+  def content_of(spot)
+    @board[spot]
+  end
+  
+  def place_marker(marker, spot)
+    @board[spot] = marker
+    @number_occupied_spots += 1
+    self
   end
     
 end
