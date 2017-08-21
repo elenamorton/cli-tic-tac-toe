a17299b6758cb88b998fe1e46a0b47bd86d02c6c
Elena Morton <elena.uleia@gmail.com>
2017-08-21 16:15:43 +0000

    adding board test for different size board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,16 +2,21 @@ require 'spec_helper'
 
 describe 'Board' do
   
-  GRID_SIZE = 3
+  GRID_WIDTH = 3
   
-  subject(:board) { Board.new(GRID_SIZE) }
+  subject(:board) { Board.new(GRID_WIDTH) }
     
   it 'expects to be initialized with size' do
-    expect(board.width).to eq GRID_SIZE 
+    expect(board.width).to eq GRID_WIDTH 
   end
   
   it 'expects number of spaces as square of grid size' do
-    expect(board.number_of_spaces).to eq GRID_SIZE * GRID_SIZE
+    expect(board.number_of_spaces).to eq GRID_WIDTH * GRID_WIDTH
+  end
+  
+  it 'expects number of spaces as square of grid size' do
+    board_four = Board.new(4)
+    expect(board_four.number_of_spaces).to eq 16
   end
     
 end
