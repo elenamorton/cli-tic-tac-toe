ff1eb1f6a07919bff502def4159efd031bc4117b
Elena Morton <elena.uleia@gmail.com>
2017-08-22 20:07:55 +0000

    refactoring game over, adding score calculator and a score hash table

--- lib/game.rb
+++ lib/game.rb
@@ -2,22 +2,21 @@ require_relative 'board'
 
 class Game
     
-  attr_accessor :outgoing, :incoming
+  attr_accessor :outgoing, :incoming, :score_table
   
   O_MARKER = "O"
   X_MARKER = "X"
-  O_SCORE = -1
-  X_SCORE = 1
+  SCORE_DEFAULT = { X_MARKER.to_sym => 1, O_MARKER.to_sym => -1 }
 
   BOARD_WIDTH = 3
     
-  def initialize(outgoing, incoming)
+  def initialize(outgoing=$stdout, incoming=$stdin)
     @board_play = Board.new(BOARD_WIDTH)
     @board = @board_play.board
     @com = X_MARKER # the computer's marker
     @hum = O_MARKER # the user's marker
-    @score_table = Hash.new {0}
-    
+    @score_table = default_score_table
+
     self.outgoing = outgoing
     self.incoming = incoming
   
@@ -29,13 +28,13 @@ class Game
     valid_moves = @board.map{ |move| move.to_i}
     
     # loop through until the game was won or tied
-    until game_is_over(@board) || tie(@board)
+    until game_is_over?(@board) || tie(@board)
     
       move = get_human_spot(valid_moves)
       @board= update_board(@board_play, @hum, move).board
       valid_moves.delete(move)
       
-      if !game_is_over(@board) && !tie(@board)
+      if !game_is_over?(@board) && !tie(@board)
         eval_board
       end
       
@@ -84,13 +83,13 @@ class Game
     end
     available_spaces.each do |as|
       board[as.to_i] = @com
-      if game_is_over(board)
+      if game_is_over?(board)
         best_move = as.to_i
         board[as.to_i] = as
         return best_move
       else
         board[as.to_i] = @hum
-        if game_is_over(board)
+        if game_is_over?(board)
           best_move = as.to_i
           board[as.to_i] = as
           return best_move
@@ -107,18 +106,20 @@ class Game
     end
   end
 
-  def game_is_over(b)
-
-    [b[0], b[1], b[2]].uniq.length == 1 ||
-    [b[3], b[4], b[5]].uniq.length == 1 ||
-    [b[6], b[7], b[8]].uniq.length == 1 ||
-    [b[0], b[3], b[6]].uniq.length == 1 ||
-    [b[1], b[4], b[7]].uniq.length == 1 ||
-    [b[2], b[5], b[8]].uniq.length == 1 ||
-    [b[0], b[4], b[8]].uniq.length == 1 ||
-    [b[2], b[4], b[6]].uniq.length == 1
+  def game_is_over?(b)
+    @score_table.has_value?(BOARD_WIDTH) || @score_table.has_value?(-BOARD_WIDTH)
   end
 
+  def calculate_score(spot, marker)
+    row = spot / BOARD_WIDTH
+    col = spot % BOARD_WIDTH
+    @score_table[:D] += SCORE_DEFAULT[marker.to_sym] if row == col
+    @score_table[:antiD] += SCORE_DEFAULT[marker.to_sym] if (row + col) == BOARD_WIDTH - 1
+    @score_table[("R" + row.to_s).to_sym] = row
+    @score_table[("C" + col.to_s).to_sym] = col
+    @score_table
+  end
+  
   
   private
   
@@ -142,8 +143,17 @@ class Game
     b.all? { |s| s == X_MARKER || s == O_MARKER }
   end
 
-  def calculate_score(b)
-    
+  def default_score_table
+    @score_table = Hash.new(0)
+    @score_table.store(:D, 0)
+    @score_table.store(:antiD, 0)
+    BOARD_WIDTH.times do |row|
+      @score_table.store(("R" + row.to_s).to_sym, 0)
+    end
+    BOARD_WIDTH.times do |col|
+      @score_table.store(("C" + col.to_s).to_sym, 0)
+    end
+    @score_table
   end
   
 end
