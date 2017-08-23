481444ce5d16849e25fa06205883ab1c9d1a6ee9
Elena Morton <elena.uleia@gmail.com>
2017-08-23 10:04:11 +0000

    adding test for calculating a tie score, correct prev#ious score results

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,7 +8,7 @@ describe 'Game' do
   describe '#score' do
     
     it 'calculates score for an empty board' do
-      expect(game.calculate_score(3, Game::X_MARKER)).to eq ({:D=>0, :antiD=>0, :R0=>0, :R1=>1, :R2=>0, :C0=>0, :C1=>0, :C2=>0})
+      expect(game.calculate_score(3, Game::X_MARKER)).to eq ({:D=>0, :antiD=>0, :R0=>0, :R1=>1, :R2=>0, :C0=>1, :C1=>0, :C2=>0})
        expect(game.game_is_over?).to be false
     end
   
@@ -19,7 +19,7 @@ describe 'Game' do
       game.calculate_score(5, Game::X_MARKER)
       game.calculate_score(8, Game::O_MARKER)
       game.calculate_score(7, Game::X_MARKER)
-      expect(game.calculate_score(6, Game::O_MARKER)).to eq ({:D=>-1, :antiD=>-3, :R0=>0, :R1=>1, :R2=>2, :C0=>0, :C1=>1, :C2=>2})
+      expect(game.calculate_score(6, Game::O_MARKER)).to eq ({:D=>-1, :antiD=>-3, :R0=>0, :R1=>0, :R2=>-1, :C0=>0, :C1=>0, :C2=>-1})
       expect(game.game_is_over?).to be true
     end
     
@@ -32,8 +32,7 @@ describe 'Game' do
       game.calculate_score(6, Game::X_MARKER)
       game.calculate_score(7, Game::O_MARKER)
       game.calculate_score(5, Game::X_MARKER)
-      expect(game.calculate_score(8, Game::O_MARKER)).to eq ({:D=>1, :antiD=>1, :R0=>0, :R1=>1, :R2=>2, :C0=>0, :C1=>1, :C2=>2})
-      expect(game.game_is_over?).to be true
+      expect(game.calculate_score(8, Game::O_MARKER)).to eq ({:D=>1, :antiD=>1, :R0=>-1, :R1=>1, :R2=>-1, :C0=>1, :C1=>-1, :C2=>-1})
     end
     
   end
