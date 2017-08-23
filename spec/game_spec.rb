68084dffece8151cedced4d7c35656cfe398fb04
Elena Morton <elena.uleia@gmail.com>
2017-08-23 14:38:31 +0000

    removed content to scorer test file, due to functionality move

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,35 +5,8 @@ describe 'Game' do
   subject(:game) { Game.new }
 
     
-  describe '#score' do
+  describe 'playing' do
     
-    it 'calculates score for an empty board' do
-      expect(game.calculate_score(3, Game::X_MARKER)).to eq ({:D=>0, :antiD=>0, :R0=>0, :R1=>1, :R2=>0, :C0=>1, :C1=>0, :C2=>0})
-       expect(game.game_is_over?).to be false
-    end
-  
-    it 'calculates score for "O" wins and "X" loses game ' do
-      game.calculate_score(4, Game::O_MARKER)
-      game.calculate_score(0, Game::X_MARKER)
-      game.calculate_score(2, Game::O_MARKER)
-      game.calculate_score(5, Game::X_MARKER)
-      game.calculate_score(8, Game::O_MARKER)
-      game.calculate_score(7, Game::X_MARKER)
-      expect(game.calculate_score(6, Game::O_MARKER)).to eq ({:D=>-1, :antiD=>-3, :R0=>0, :R1=>0, :R2=>-1, :C0=>0, :C1=>0, :C2=>-1})
-      expect(game.game_is_over?).to be true
-    end
-    
-    it 'calculates score for a tie game' do
-      game.calculate_score(1, Game::O_MARKER)
-      game.calculate_score(4, Game::X_MARKER)
-      game.calculate_score(2, Game::O_MARKER)
-      game.calculate_score(0, Game::X_MARKER)
-      game.calculate_score(3, Game::O_MARKER)
-      game.calculate_score(6, Game::X_MARKER)
-      game.calculate_score(7, Game::O_MARKER)
-      game.calculate_score(5, Game::X_MARKER)
-      expect(game.calculate_score(8, Game::O_MARKER)).to eq ({:D=>1, :antiD=>1, :R0=>-1, :R1=>1, :R2=>-1, :C0=>1, :C1=>-1, :C2=>-1})
-    end
     
   end
   
