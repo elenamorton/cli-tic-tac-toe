673783c839df2f7cfd157ae81d16d7a3db5d3942
Elena Morton <elena.uleia@gmail.com>
2017-08-23 15:51:01 +0000

    adding test for user entry that is already marked, corrected a test case title

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -8,11 +8,12 @@ require 'stringio'
 describe Game do
   
   let(:board_play) { Board.new(Game::BOARD_WIDTH) }
-    
+  let(:scorer) { Scorer.new({:width => Game::BOARD_WIDTH, :x_marker => Game::X_MARKER, :o_marker => Game::O_MARKER}) }
+  
   def io(input='')
     incoming = StringIO.new(input)
     @outgoing = StringIO.new
-    described_class.new(@outgoing, incoming) 
+    described_class.new(@outgoing, incoming)
   end
     
   def output
@@ -20,7 +21,7 @@ describe Game do
   end
   
   describe '#display_board' do
-    
+  
     it 'displaying an starting board of 0 to 8 chars' do
       io.display_board(board_play)
       expect(output).to eq " 0 | 1 | 2 \n" \
@@ -45,9 +46,9 @@ describe Game do
                           " #{marker} | 7 | 8 \n"
     end
     
-    it 'inside the board and on an invalid spaces: 1, 3, and 8 ' do
+    it 'inside the board and on an valid spaces: 1, 3, and 8 ' do
       marker = Game::O_MARKER
-      spot = io("1\n3\n\8\n6\n").get_human_spot([2,6])
+      spot = io("1\n3\n8\n6\n").get_human_spot([2,6])
       io.update_board(board_play, marker, spot)
       io.display_board(board_play)
       expect(output).to eq " 0 | 1 | 2 \n" \
@@ -55,6 +56,20 @@ describe Game do
                           " 3 | 4 | 5 \n" \
                           "===+===+===\n" \
                           " #{marker} | 7 | 8 \n"
+    end
+    
+    it 'inside the board and on an invalid space' do
+      marker = Game::O_MARKER
+      spot = io("6\n").get_human_spot([6])
+      io.update_board(board_play, marker, spot)
+      io.display_board(board_play)
+      expect(output).to eq " 0 | 1 | 2 \n" \
+                          "===+===+===\n" \
+                          " 3 | 4 | 5 \n" \
+                          "===+===+===\n" \
+                          " #{marker} | 7 | 8 \n"
+      io("1\n6\n").get_human_spot([6])
+      expect(output).to include "You've entered 1, which is invalid\n"
     end
   end
 
