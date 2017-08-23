ade25894b83357b00ca1fed38569381a7ffbf547
Elena Morton <elena.uleia@gmail.com>
2017-08-23 15:19:09 +0000

    adding tests for tie/no-tie, correcting titles for 3 tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,24 +8,49 @@ describe 'Board' do
     expect(board.width).to eq Board::GRID_WIDTH 
   end
   
-  it 'expects number of spaces as square of grid size' do
+  it 'expects for grid 3 number of spaces as 9' do
     expect(board.number_of_spots).to eq Board::GRID_WIDTH**2
   end
   
-  it 'expects number of spaces as square of grid size' do
+  it 'expects fir grid 4 number of spaces as 16' do
     board_four = Board.new(4)
     expect(board_four.number_of_spots).to eq 16
   end
   
-  it 'shows the content of a board spot' do
-    expect(board.content_of(3)).to eq "3"
+  describe 'board spots changes' do
+    
+    it 'shows the default content of a board spot' do
+      expect(board.content_of(3)).to eq "3"
+    end
+  
+    it 'updates board with a marker' do
+      board.place_marker("X", 4)
+      expect(board.content_of(4)).to eq "X"
+    end
+    
   end
   
-  it 'updates board with a marker' do
-    board.place_marker("X", 4)
-    expect(board.content_of(4)).to eq "X"
+  describe 'checks for tie' do
+    it 'updates one spot a marker, no tie' do
+      board.place_marker("X", 4)
+      expect(board.content_of(4)).to eq "X"
+      expect(board.tie?).to be false
+    end
+    
+    it 'updates all spots resulting in a tie' do
+      board.place_marker("O", 1)
+      board.place_marker("X", 0)
+      board.place_marker("O", 2)
+      board.place_marker("X", 4)
+      board.place_marker("O", 3)
+      board.place_marker("X", 5)
+      board.place_marker("O", 6)
+      board.place_marker("X", 7)
+      board.place_marker("O", 8)
+      expect(board.tie?).to be true
+    end
   end
-  
+   
   it 'stringifies an empty board with no markers' do
     expect(board.board_stringify).to eq " 0 | 1 | 2 \n" \
                           "===+===+===\n" \
