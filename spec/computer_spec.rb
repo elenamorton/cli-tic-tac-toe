88fe4e26283548241cd10b34b1e278c75b695b77
Elena Morton <elena.uleia@gmail.com>
2017-08-24 13:47:24 +0000

    adding test to check default name

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -3,10 +3,15 @@ require 'spec_helper'
 describe 'Computer' do
   
   subject(:computer) { Computer.new(parameters) }
-  let(:parameters) { { :marker => "X", :width => 3 } }
+  let(:scorer) { double :scorer }
+  let(:parameters) { { :marker => "X", :width => 3, :scorer => scorer, :depth => 0 } }
     
   it 'expects to be initialized with a marker ' do
     expect(computer.marker).to eq "X" 
+  end
+  
+  it 'expects to be initialized with a name ' do
+    expect(computer.name).to eq :computer
   end
   
 end
