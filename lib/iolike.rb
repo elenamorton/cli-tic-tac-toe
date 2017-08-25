0dfe11bc0cba643bcf7cb457de2f181ad2242c0f
Elena Morton <elena.uleia@gmail.com>
2017-08-25 21:49:10 +0000

    extracting input and output handling to IOlike module

---
+++ lib/iolike.rb
@@ -0,0 +1,38 @@
+module Iolike
+  
+  def outgoing
+    Iolike.outgoing
+  end
+  
+  def self.outgoing
+    @outgoing ||= $stdout
+  end
+  
+  def incoming
+    Iolike.incoming
+  end
+  
+  def self.incoming
+    @incoming ||= $stdin
+  end
+  
+  def get_input(message_prompt, validation)
+    prompt message_prompt
+    input = incoming.gets.chomp
+    return input if input =~ validation
+    invalid_data input
+    get_input(message_prompt, validation)
+  end
+  
+  private
+  
+  def invalid_data(data)
+    outgoing.puts "You've entered #{data}, which is invalid"
+  end
+  
+  def prompt(message)
+    outgoing.print "#{message}: "
+  end
+  
+end
+  
