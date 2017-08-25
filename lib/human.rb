3af206d81d38f60aa52dbceb6bbeea5367e63e3f
Elena Morton <elena.uleia@gmail.com>
2017-08-25 21:48:26 +0000

    refactoring, adding module IOlike into Human class, and TDD

--- lib/human.rb
+++ lib/human.rb
@@ -1,12 +1,25 @@
+require_relative 'iolike'
+
 class Human
-  
-  attr_reader :marker, :spot, :name
     
-  def initialize(parameters)
+  attr_reader :marker, :spot, :name, :incoming, :outgoing
+  include Iolike
+    
+  def initialize(outgoing, incoming, parameters)
     @width = parameters[:width]
     @marker = parameters[:marker]
     @scorer = parameters[:scorer]
     @name = :human
+    
+    @outgoing = outgoing
+    @incoming = incoming
+
   end
+  
+  
+  def get_next_move(valid_spots)
+    get_input("Enter your move", /\A[#{valid_spots.join('')}]\z/).to_i
+  end
+
   
 end
--- spec/human_spec.rb
+++ spec/human_spec.rb
@@ -1,18 +1,35 @@
 require 'spec_helper'
+require File.join(File.dirname(__FILE__), '../lib/', 'human.rb')
+require File.join(File.dirname(__FILE__), '../lib/', 'scorer.rb')
 
 describe 'Human' do
   
-  subject(:human) { Human.new(parameters) }
-  let(:parameters) { { :marker => "O", :width => 3, :scorer => scorer} }
-  let(:scorer) { double :scorer }
-  let(:board_play) { double :board }
+  #subject(:human) { Human.new(parameters) }
+  let(:scorer) { Scorer.new({:width => 3, :x_marker => 'X', :o_marker => 'O'}) }
+  let(:parameters) { { :marker => 'O', :width => 3, :scorer => scorer} }
+
+  def humanio(input='')
+    incoming = StringIO.new(input)
+    @outgoing = StringIO.new
+    Human.new(@outgoing, incoming, parameters)
+  end
     
+  def output
+    @outgoing.string
+  end
+  
   it 'expects to be initialized with a marker ' do
-    expect(human.marker).to eq "O" 
+    expect(humanio.marker).to eq 'O' 
   end
   
   it 'expects to be initialized with a name ' do
-    expect(human.name).to eq :human
+    expect(humanio.name).to eq :human
+  end
+  
+  it 'returns a number as input' do
+    #allow($stdin).to receive(:gets).and_return('2\n')
+    move = humanio('2').get_next_move([0,2,3,4])
+    expect(move).to eq 2
   end
   
 end
