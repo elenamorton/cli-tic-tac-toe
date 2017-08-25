a9933a7afbacda47b1b366354122fbb1bffcae2c
Elena Morton <elena.uleia@gmail.com>
2017-08-25 23:34:42 +0000

    adding in all class files, adding the new Human class API call for next move.

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -1,9 +1,16 @@
-require 'spec_helper'
-require 'stringio'
-
+####################################################
 # As a user
 # So as I can have a nice time
 # I'd like to be able to play tic-tac-toe game
+####################################################
+
+require 'spec_helper'
+require 'stringio'
+require File.join(File.dirname(__FILE__), '../../lib/', 'game.rb')
+require File.join(File.dirname(__FILE__), '../../lib/', 'board.rb')
+require File.join(File.dirname(__FILE__), '../../lib/', 'human.rb')
+require File.join(File.dirname(__FILE__), '../../lib/', 'scorer.rb')
+
 
 describe Game do
   
@@ -20,6 +27,8 @@ describe Game do
     @outgoing.string
   end
   
+  let(:human) { Human.new({:width => Game::BOARD_WIDTH, :o_marker => Game::O_MARKER, :scorer => scorer, :outgoing => io.outgoing, :incoming => io.incoming }) }
+    
   describe '#display_board' do
   
     it 'displaying an starting board of 0 to 8 chars' do
@@ -36,8 +45,7 @@ describe Game do
     
     it 'inside the board and on unoccupied space' do
       marker = Game::O_MARKER
-      spot = io('6').get_human_spot([2,6])
-      #io.update_board(board_play, marker, spot)
+      spot = io('6').human.get_next_move([2,6])
       board_play.place_marker(marker, spot)
       io.display_board(board_play)
       expect(output).to eq " 0 | 1 | 2 \n" \
@@ -49,7 +57,7 @@ describe Game do
     
     it 'inside the board and on an valid spaces: 1, 3, and 8 ' do
       marker = Game::O_MARKER
-      spot = io("1\n3\n8\n6\n").get_human_spot([2,6])
+      spot = io("1\n3\n8\n6\n").human.get_next_move([2,6])
       board_play.place_marker(marker, spot)
       io.display_board(board_play)
       expect(output).to eq " 0 | 1 | 2 \n" \
@@ -61,7 +69,7 @@ describe Game do
     
     it 'inside the board and on an invalid space' do
       marker = Game::O_MARKER
-      spot = io("6\n").get_human_spot([6])
+      spot = io("6\n").human.get_next_move([6])
       board_play.place_marker(marker, spot)
       io.display_board(board_play)
       expect(output).to eq " 0 | 1 | 2 \n" \
@@ -69,13 +77,13 @@ describe Game do
                           " 3 | 4 | 5 \n" \
                           "===+===+===\n" \
                           " #{marker} | 7 | 8 \n"
-      io("1\n6\n").get_human_spot([6])
+      io("1\n6\n").human.get_next_move([6])
       expect(output).to include "You've entered 1, which is invalid\n"
     end
   end
 
   it 'play a full game, user starts in a corner' do
-    io("8\n6\n1\n5\n0\n3\n").start_game
+    io("8\n6\n1\n5\n0\n3\n7\n").start_game
     expect(output).to include Game::O_MARKER
     expect(output).to include Game::X_MARKER
     expect(output).to include 'Game over'
@@ -83,7 +91,7 @@ describe Game do
   end
     
   it 'play a full game, user starts in the center' do
-    io("4\n8\n2\n6\n7\n5\n").start_game
+    io("4\n8\n2\n6\n7\n5\n3\n").start_game
     expect(output).to include Game::O_MARKER
     expect(output).to include Game::X_MARKER
     expect(output).to include 'Game over'
