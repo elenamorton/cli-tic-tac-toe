00722d6b26de7285547c76e7b67bee578ac862d9
Elena Morton <elena.uleia@gmail.com>
2017-08-27 17:38:26 +0000

    corrected game unit tests to show user input, added support for Feature 1: user chooses defficulty level

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,15 +4,18 @@ require File.join(File.dirname(__FILE__), '../lib/', 'game.rb')
 describe 'Game unit tests' do
   
   def io(input='')
-    incoming = StringIO.new(input)
+    @incoming = StringIO.new(input)
     @outgoing = StringIO.new
-    Game.new(@outgoing, incoming)
+    Game.new(@outgoing, @incoming)
   end
     
   def output
     @outgoing.string
   end
 
+  def input
+    @incoming.string
+  end
     
   it 'has an empty players list' do
     expect(io.players.empty?).to be true
@@ -22,20 +25,31 @@ describe 'Game unit tests' do
   
     it 'user chooses both players as Human' do
       io("human\nhuman\no\nx\n1\n").user_setup_game
-      expect(output).to include 'human'
+      expect(input).to include 'human'
     end
   
     it 'user chooses both players as Computer' do
-      io("computer\ncomputer\no\nx\n2\n").user_setup_game
-      expect(output).to include 'computer'
+      io("computer\ncomputer\no\nx\nmedium\n2\n").user_setup_game
+      expect(input).to include 'computer'
     end
     
-    it 'user chooses both players as Computer' do
-      io("human\ncomputer\no\nx\n2\n").user_setup_game
-      expect(output).to include 'human'
-      expect(output).to include 'computer'
+    it 'user chooses one player as Human and the other as Computer' do
+      io("human\ncomputer\no\nx\neasy\n2\n").user_setup_game
+      expect(input).to include 'human'
+      expect(input).to include 'computer'
     end
   end
+  
+  describe 'user chooses players order' do
+    it 'current player is choosen as 1st player' do
+      io("human\ncomputer\no\nx\neasy\n1\n").user_setup_game
+      expect(input).to include '1'
+    end
 
+    it 'current player is choosen as 2nd player' do
+      io("human\ncomputer\no\nx\neasy\n2\n").user_setup_game
+      expect(input).to include '2'
+    end
+  end
   
 end
